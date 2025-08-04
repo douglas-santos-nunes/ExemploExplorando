894e828fcadd9e4d2753cdb1a31d8a22c76fb9f1
Douglas Nunes <douglasnunesprogramador@gmail.com>
2025-08-04 20:56:13 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p = new Pessoa("Douglas","Nunes");
+
+//p.Nome="Douglas";
+//p.Sobrenome = "Nunes";
+
+Pessoa p2 = new Pessoa("Teste","Testenildo");
+
+//p2.Nome="Teste";
+//p2.Sobrenome = "Testenildo";
+
+Curso cursoDeIngles = new Curso();
+
+cursoDeIngles.Nome = "Curso de Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
