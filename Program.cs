2197862a6834992df552d42601e98f6be65152bc
Douglas Nunes <douglasnunesprogramador@gmail.com>
2025-08-04 21:38:59 -0300

    Lendo arquivo txt

--- Program.cs
+++ Program.cs
@@ -1,21 +1,36 @@
 ﻿using ExemploExplorando.Models;
 
-Pessoa p = new Pessoa("Douglas","Nunes");
+string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
 
-//p.Nome="Douglas";
-//p.Sobrenome = "Nunes";
+foreach (string linha in linhas)
+{
+    Console.WriteLine(linha);
+}
 
-Pessoa p2 = new Pessoa("Teste","Testenildo");
 
-//p2.Nome="Teste";
-//p2.Sobrenome = "Testenildo";
 
-Curso cursoDeIngles = new Curso();
 
-cursoDeIngles.Nome = "Curso de Inglês";
-cursoDeIngles.Alunos = new List<Pessoa>();
 
-cursoDeIngles.AdicionarAluno(p);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.ListarAlunos();
+
+
+
+
+// Pessoa p = new Pessoa("Douglas","Nunes");
+
+// //p.Nome="Douglas";
+// //p.Sobrenome = "Nunes";
+
+// Pessoa p2 = new Pessoa("Teste","Testenildo");
+
+// //p2.Nome="Teste";
+// //p2.Sobrenome = "Testenildo";
+
+// Curso cursoDeIngles = new Curso();
+
+// cursoDeIngles.Nome = "Curso de Inglês";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AdicionarAluno(p);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.ListarAlunos();
 
