d962ae6e297542288f1ac5129f8c1c1cf2359fa5
Douglas Nunes <douglasnunesprogramador@gmail.com>
2025-08-04 22:31:05 -0300

    Exmplos de Filas, pilhas e dictionary

--- Program.cs
+++ Program.cs
@@ -1,26 +1,133 @@
 ﻿using ExemploExplorando.Models;
+// using System.Globalization;
 
-try
-{
-    string[] linhas = File.ReadAllLines("Arquivos/arquivo_Leitura.txt");
+// Dictionary<string, string> estados = new Dictionary<string, string>();
 
-    foreach (string linha in linhas)
-    {
-        Console.WriteLine(linha);
-    }
-}
-catch (FileNotFoundException ex)
-{
-    Console.WriteLine($"Arquivo não encontrado. {ex.Message}");
-}
-catch (DirectoryNotFoundException ex)
-{
-    Console.WriteLine($"Caminho da pasta não encontrado. {ex.Message}");
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Ocorreu uma exceção. {ex.Message}");
-}
+// estados.Add("RS", "Rio Grande do Sul");
+// estados.Add("SP", "São Paulo");
+// estados.Add("MG", "Minas Gerais");
+
+// foreach (var item in estados)
+// {
+//     Console.WriteLine($"{item.Key}, {item.Value}");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Stack<int> pilha = new Stack<int>();
+
+// pilha.Push(2);
+// pilha.Push(4);
+// pilha.Push(6);
+// pilha.Push(8);
+
+// foreach (int item in pilha)
+// {
+//     System.Console.WriteLine(item);
+// }
+
+// pilha.Pop();
+
+// foreach (int item in pilha)
+// {
+//     System.Console.WriteLine(item);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Queue<int> fila = new Queue<int>();
+
+// fila.Enqueue(2);
+// fila.Enqueue(4);
+// fila.Enqueue(6);
+// fila.Enqueue(8);
+
+// foreach (int item in fila)
+// {
+//     System.Console.WriteLine(item);
+// }
+
+// fila.Dequeue();
+
+// foreach (int item in fila)
+// {
+//     System.Console.WriteLine(item);
+// }
+
+
+
+
+
+
+
+// try
+// {
+//     string[] linhas = File.ReadAllLines("Arquivos/arquivo_Leitura.txt");
+
+//     foreach (string linha in linhas)
+//     {
+//         Console.WriteLine(linha);
+//     }
+// }
+// catch (FileNotFoundException ex)
+// {
+//     Console.WriteLine($"Arquivo não encontrado. {ex.Message}");
+// }
+// catch (DirectoryNotFoundException ex)
+// {
+//     Console.WriteLine($"Caminho da pasta não encontrado. {ex.Message}");
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine($"Ocorreu uma exceção. {ex.Message}");
+// }
+// finally
+// {
+//     Console.WriteLine("Chegou até aqui");
+// }
 
 
 
